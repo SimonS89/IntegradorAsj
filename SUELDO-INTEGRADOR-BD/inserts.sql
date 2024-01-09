4b02cec6f3ab843a881ab580dda1371f809b7b97
SimonS89 <simonrsueldo@gmail.com>
2024-01-09 00:54:29 -0300

    bd integrador

---
+++ SUELDO-INTEGRADOR-BD/inserts.sql
@@ -0,0 +1,11 @@
+use integrador;
+
+--insert into paises (nombre) values ('Argentina'),('Uruguay');
+--insert into provincias (nombre,pais_id) values ('Buenos Aires',1),('Córdoba',1),('Mendoza',1),('Montevideo',2);
+--insert into tipos_iva (tipo) values ('Consumidor final'),('Exento'),('Responsable inscripto');
+--insert into rubros (rubro) values ('Tecnología'),('Alimentación'),('Moda'),('Salud'),('Educación');
+--insert into categorias (categoria) values ('Tecnología'),('Alimentación'),('Moda'),('Salud'),('Educación');
+--insert into estados_orden (estado) values ('Entregado'),('Cancelado'),('Pendiente');
+--insert into domicilios (calle,numero,codigo_postal,localidad,provincia_id) values ('142','1123','1900','La Plata',1),('Estados Unidos','1183','1102','San Telmo',1);
+--insert into personas_contacto (nombre,apellido,email,telefono,puesto) values ('Simón','Sueldo','ssueldo@asjservicios.com','2215578552','Jefe de ventas'),('Beatriz','Bocacini','bbeatriz@gmail.com','2215236879','Encargada de Marketing');
+select * from personas_contacto;
