53c17b52b006ca73aba02b7d4d26a87112e66100
SimonS89 <simonrsueldo@gmail.com>
2024-01-14 21:43:13 -0300

    password show

--- SUELDO-INTEGRADOR-ANGULAR/my-app/src/app/components/login/login.component.ts
+++ SUELDO-INTEGRADOR-ANGULAR/my-app/src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  resetPasswordState() {
+    throw new Error('Method not implemented.');
+  }
+  togglePassword() {
+    throw new Error('Method not implemented.');
+  }
   email!: string;
   password!: string;
+  faeye = faEye;
+  mostrarPassword: boolean = false;
   constructor(private router: Router) {}
   onSubmit(form: NgForm) {
     this.router.navigate(['index']);
+  }
+
+  cambiarVisibilidad() {
+    this.mostrarPassword = !this.mostrarPassword;
+  }
+
+  resetearEstado() {
+    this.mostrarPassword = false;
   }
 }
