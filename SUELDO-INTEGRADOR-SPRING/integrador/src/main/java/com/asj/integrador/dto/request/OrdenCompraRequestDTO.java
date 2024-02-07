5cae0903ae3504db81d9f5ca5acb24e1e9d63c88
SimonS89 <simonrsueldo@gmail.com>
2024-02-07 16:47:29 -0300

    version funcionando sin seguridad

--- SUELDO-INTEGRADOR-SPRING/integrador/src/main/java/com/asj/integrador/dto/request/OrdenCompraRequestDTO.java
+++ SUELDO-INTEGRADOR-SPRING/integrador/src/main/java/com/asj/integrador/dto/request/OrdenCompraRequestDTO.java
@@ -3,10 +3,7 @@ package com.asj.integrador.dto.request;
 import com.asj.integrador.model.DetalleOrden;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -35,6 +32,7 @@ public class OrdenCompraRequestDTO {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaEmision;
     @NotNull
+    @Future
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaEntrega;
     private List<DetalleOrden> detallesOrden;
